e9e30266003d839339025725a7da1a30c7e475fb
Gonzalo <gonzalogomez22@gmail.com>
2021-02-06 17:57:15 -0300

    *ngif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'front-end';
-  pelicula ={
-  nombre:'spiderman',
-  fechaDeLanzamiento: new Date()
-  };
-
-  duplicarValor(valor: number): number{
-    return valor *2;
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.series = [{
+        nombre: 'Walking Dead',
+        fechaLanzamiento: new Date(26 / 12 / 20),
+        precio: 320
+      },
+      {
+        nombre: 'Braking Bad',
+        fechaLanzamiento: new Date(),
+        precio: 500
+      }]
+    }, 1000);
   }
 
-  series = [{
-   nombre:'Walking Dead',
-   fechaLanzamiento: new Date(26/12/20),
-   precio: 320
-  },
-  {
-    nombre:'Braking Bad',
-    fechaLanzamiento: new Date(),
-    precio: 500
-  }]
+  series: any;
+
+  title = 'front-end';
+
+  pelicula = {
+    nombre: 'spiderman',
+    fechaDeLanzamiento: new Date()
+  };
+
+  duplicarValor(valor: number): number {
+    return valor * 2;
+  }
 }
