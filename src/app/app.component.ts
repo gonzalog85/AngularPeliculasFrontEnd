2f40ce8362835b76fe2f544f539ec1367ee39eba
Gonzalo <gonzalogomez22@gmail.com>
2021-02-06 20:51:17 -0300

    Creacion de componente listado-personas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,21 +8,42 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   ngOnInit(): void {
     setTimeout(() => {
-      this.series = [{
-        nombre: 'Walking Dead',
+      this.peliculasEstrenos = [{
+        nombre: 'Rocky',
         fechaLanzamiento: new Date(26 / 12 / 20),
         precio: 320
       },
       {
-        nombre: 'Braking Bad',
+        nombre: 'Rambo',
+        fechaLanzamiento: new Date(),
+        precio: 500
+      }]
+    }, 1000);
+
+    setTimeout(() => {
+      this.proximosLanzamientos = [{
+        nombre: 'Walking dead the movie',
+        fechaLanzamiento: new Date(26 / 12 / 20),
+        precio: 320
+      },
+      {
+        nombre: 'Los 4 fantasticos',
+        fechaLanzamiento: new Date(),
+        precio: 500
+      },
+      {
+        nombre: 'Thor',
         fechaLanzamiento: new Date(),
         precio: 500
       }]
     }, 1000);
   }
 
-  series: any;
+  peliculasEstrenos:any;
+  proximosLanzamientos: any;
 
+
+/*
   title = 'front-end';
 
   pelicula = {
@@ -33,4 +54,5 @@ export class AppComponent implements OnInit {
   duplicarValor(valor: number): number {
     return valor * 2;
   }
+  */
 }
