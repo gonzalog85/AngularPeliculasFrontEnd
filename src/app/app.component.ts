dc33670d28ba143795b19beb036289af011074ec
Gonzalo <gonzalogomez22@gmail.com>
2021-02-08 15:21:27 -0300

    Aplicando concepto de routing a la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,59 +5,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  ngOnInit(): void {
-    setTimeout(() => {
-      this.peliculasEstrenos = [{
-        nombre: 'Rocky',
-        fechaLanzamiento: new Date(26 / 12 / 20),
-        precio: 320,
-        poster:"https://m.media-amazon.com/images/M/MV5BMTY5MDMzODUyOF5BMl5BanBnXkFtZTcwMTQ3NTMyNA@@._V1_UX182_CR0,0,182,268_AL_.jpg"
-      },
-      {
-        nombre: 'Rambo',
-        fechaLanzamiento: new Date(),
-        precio: 500,
-        poster:"https://m.media-amazon.com/images/M/MV5BZWFkY2I1ZDAtNmZhNS00NjVlLWJiMGQtMGQ1ZmM0ZDA5ODg5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"
-      }]
-    }, 1000);
+export class AppComponent{
 
-    /*setTimeout(() => {
-      this.proximosLanzamientos = [{
-        nombre: 'Walking dead the movie',
-        fechaLanzamiento: new Date(26 / 12 / 20),
-        precio: 320
-      },
-      {
-        nombre: 'Los 4 fantasticos',
-        fechaLanzamiento: new Date(),
-        precio: 500
-      },
-      {
-        nombre: 'Thor',
-        fechaLanzamiento: new Date(),
-        precio: 500
-      }]
-    }, 1000);*/
-  }
-
-  peliculasEstrenos:any;
-  proximosLanzamientos:any =[];
-
-  manejarRated(calificado:number):void{
-    alert(calificado);
-  }
-
-/*
-  title = 'front-end';
-
-  pelicula = {
-    nombre: 'spiderman',
-    fechaDeLanzamiento: new Date()
-  };
-
-  duplicarValor(valor: number): number {
-    return valor * 2;
-  }
-  */
 }
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculasEstrenos = [{
+        nombre: 'Rocky',
+        fechaLanzamiento: new Date(26 / 12 / 20),
+        precio: 320,
+        poster:"https://m.media-amazon.com/images/M/MV5BMTY5MDMzODUyOF5BMl5BanBnXkFtZTcwMTQ3NTMyNA@@._V1_UX182_CR0,0,182,268_AL_.jpg"
+      },
+      {
+        nombre: 'Rambo',
+        fechaLanzamiento: new Date(),
+        precio: 500,
+        poster:"https://m.media-amazon.com/images/M/MV5BZWFkY2I1ZDAtNmZhNS00NjVlLWJiMGQtMGQ1ZmM0ZDA5ODg5XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX182_CR0,0,182,268_AL_.jpg"
+      }]
+    }, 1000);
+
+    /*setTimeout(() => {
+      this.proximosLanzamientos = [{
+        nombre: 'Walking dead the movie',
+        fechaLanzamiento: new Date(26 / 12 / 20),
+        precio: 320
+      },
+      {
+        nombre: 'Los 4 fantasticos',
+        fechaLanzamiento: new Date(),
+        precio: 500
+      },
+      {
+        nombre: 'Thor',
+        fechaLanzamiento: new Date(),
+        precio: 500
+      }]
+    }, 1000);*/
+  }
+
+  peliculasEstrenos:any;
+  proximosLanzamientos:any =[];
+
+  manejarRated(calificado:number):void{
+    alert(calificado);
+  }
+
+/*
+  title = 'front-end';
+
+  pelicula = {
+    nombre: 'spiderman',
+    fechaDeLanzamiento: new Date()
+  };
+
+  duplicarValor(valor: number): number {
+    return valor * 2;
+  }
+  */
+
+}
