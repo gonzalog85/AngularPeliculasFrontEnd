ea8611571e6ac209b07f2126d82c9e689c594bfd
Gonzalo <gonzalogomez22@gmail.com>
2021-02-09 16:49:18 -0300

    Creacion formulario genero y validaciones para el campo nombre

--- src/app/generos/crear-genero/crear-genero.component.ts
+++ src/app/generos/crear-genero/crear-genero.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { primeraLetraMayuscula } from 'src/app/utilidades/validadores/primeraLetraMayuscula';
 
 @Component({
   selector: 'app-crear-genero',
@@ -8,17 +10,37 @@ import { Router } from '@angular/router';
 })
 export class CrearGeneroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private formBuilder: FormBuilder) { }
+
+  form!: FormGroup;
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      nombre: ['', {
+        validators: [Validators.required, Validators.minLength(3), primeraLetraMayuscula()]
+      }]
+    });
   }
 
-  guardarCambios(){
-
+  guardarCambios() {
     // ... guardar los cambios en la base de dato - comunicacion con la web api
-
     this.router.navigate(['/generos']);
+  }
 
+  obtenerErrorCampoNombre() {
+    var campo = this.form.get('nombre');
+    if (campo?.hasError('required')) {
+      return 'El campo nombre es requerido';
+    }
+
+    if (campo?.hasError('minlength')) {
+      return "La longitud minima es de 3 caracteres";
+    }
+
+    if (campo?.hasError('primeraLetraMayuscula')) {
+      return campo.getError('primeraLetraMayuscula').mensaje;
+    }
+    return '';
   }
 
 }
